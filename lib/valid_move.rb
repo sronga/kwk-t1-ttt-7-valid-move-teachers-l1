ef966f2b857956a58526f5490272a6d9e926e3c1
sronga <sr899@georgetown.edu>
2018-06-06 15:26:09 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-if position_taken? == true
+if position_taken?(board, index) == true
     false
 #  elsif position_taken? == false
 #    ( if index <= 8 && index >= 0  true
